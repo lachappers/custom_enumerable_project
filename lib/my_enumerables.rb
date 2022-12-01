ca5d3a7e23fa3acd86e726e302256cfbc8479642
lachappers <116145040+lachappers@users.noreply.github.com>
2022-12-01 16:30:59 +0000

    each method added to array class

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,11 @@
 module Enumerable
   # Your code goes here
+  # def my_each 
+  #   for i in self do
+
+  #   end
+  #   self
+  # end
 end
 
 # You will first have to define my_each
@@ -7,5 +13,18 @@ end
 # your enumerable module will have access
 # to this method
 class Array
+  include Enumerable
+
+
   # Define my_each here
+  def my_each
+    for element in self do
+    yield element
+    end
+  end
+
+
+end
+class Hash
+  include Enumerable
 end
