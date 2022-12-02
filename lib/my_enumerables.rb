93481e572db2b66c67be4fd6fa8f2114fcafc114
lachappers <116145040+lachappers@users.noreply.github.com>
2022-12-02 13:35:12 +0000

    partially completed

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,12 +1,51 @@
 module Enumerable
   # Your code goes here
-  # def my_each 
-  #   for i in self do
+  def my_each_with_index
+    # to_enum(:my_each)
+    index = 0
+    my_each do |element|
+      yield(element, index)
+      index +=1
+    end
+  end
 
-  #   end
-  #   self
-  # end
+  def my_select
+    result = []
+    my_each do |element|
+      result << element if yield(element)
+    end
+    result
+  end
+
+  def my_all?(&block)
+    my_each do |element|
+      return false unless block.call(element)
+    end
+    true
+  end
+
+  def my_none?(&block)
+
+    my_each do |element|
+      if yield(element)
+        return false
+      else true
+      end
+    end
+    
+  end
+
+  def my_map(&block)
+    result = []
+    my_each do |element|
+      result << block.call(element)
+    end
+    result
+  end
+
+
 end
+
 
 # You will first have to define my_each
 # on the Array class. Methods defined in
